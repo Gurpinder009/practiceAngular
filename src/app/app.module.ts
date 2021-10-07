edd647a670dc0cf449112c943291028f709fd8b5
Gurpinder <singh9464274057@gmail.com>
2021-10-07 20:47:28 +0530

    initial commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ContentComponent } from './Components/content/content.component';
+import { ContactMeComponent } from './Components/contact-me/contact-me.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContentComponent,
+    ContactMeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
